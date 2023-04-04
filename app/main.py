5cc2765b4b86c304f4536fb5e10c5b85cee0a6c0
hulacorn <61078606+uyivzola@users.noreply.github.com>
2023-04-04 18:42:02 +0500

    add vote route

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 from . import models
 from .database import engine
-from .routers import auth, posts, users
+from .routers import auth, posts, users, vote
 # interacting with DATABASE
 # copy + paste from fastapi website
 # Creating database tables using metadata in models.py and the database engine.
@@ -14,6 +14,7 @@ app = FastAPI()  # Creating a FastAPI instance called app
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 # Defining a GET route at the root (/) URL that returns a message.
 @app.get('/')
