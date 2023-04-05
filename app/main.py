3b27808a6bc56b4e321cc4cb4b42a851f8f1dce8
hulacorn <61078606+uyivzola@users.noreply.github.com>
2023-04-05 07:02:50 +0500

    added CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 # Importing necessary libraries, modules, and dependencies
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .database import engine
 from .routers import auth, posts, users, vote
+
 # interacting with DATABASE
 # copy + paste from fastapi website
 # Creating database tables using metadata in models.py and the database engine.
@@ -11,12 +13,25 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()  # Creating a FastAPI instance called app
 
+origins=["*"] # websites that can access to our api, like google, youtube
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=["*"]
+)
+
+
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
 
 # Defining a GET route at the root (/) URL that returns a message.
+
+
 @app.get('/')
 def root():
     return {"message": "Hello World!!"}
