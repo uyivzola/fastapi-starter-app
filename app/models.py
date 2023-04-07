2d2c44a29f6bb8bc285ccbba77cbf4b9ef6e7a1a
hulacorn <61078606+uyivzola@users.noreply.github.com>
2023-04-07 18:17:43 +0500

    some final changes

--- app/models.py
+++ app/models.py
@@ -1,27 +1,29 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
-from sqlalchemy.orm import relationship
 
 from .database import Base
 
 
 class Post(Base):
-    __tablename__ = 'posts'
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True, nullable=False)
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
     owner_id = Column(Integer, ForeignKey(
         "users.id", ondelete="CASCADE"), nullable=False)
+
     owner = relationship("User")
 
 
 class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True)
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
@@ -29,9 +31,8 @@ class User(Base):
 
 
 class Vote(Base):
-    __tablename__ = 'votes'
-    # id=Column(Integer, nullable=False, primary_key=True, index=True)
-    post_id = Column(Integer, ForeignKey(
-        "posts.id", ondelete="CASCADE"), nullable=False, primary_key=True)
+    __tablename__ = "votes"
     user_id = Column(Integer, ForeignKey(
-        "users.id", ondelete="CASCADE"), nullable=False, primary_key=True)
+        "users.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey(
+        "posts.id", ondelete="CASCADE"), primary_key=True)
